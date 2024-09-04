6c1fec77343bb7027866734dcc1a84a1e9f311a6
aryamaanthakur <aryamaan.thakur@outlook.com>
2024-09-04 11:35:27 +0530

    feat(te): add contract support for TE2
    Signed-off-by: aryamaanthakur <aryamaan.thakur@outlook.com>

--- fm_training_estimator/tokens/te2/te2.py
+++ fm_training_estimator/tokens/te2/te2.py
@@ -2,6 +2,7 @@
 from datasets import load_dataset
 from tqdm import tqdm
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
 # Local
 from ...config import DataArguments
@@ -19,7 +20,10 @@ class TokenEstimator2(TokenEstimator):
             dataset = load_dataset("json", data_files={"train": da.dataset})["train"]
         else:
             dataset = load_dataset(
-                da.dataset, name=da.dataset_config_name, split=da.dataset_split
+                da.dataset,
+                name=da.dataset_config_name,
+                split=da.dataset_split,
+                trust_remote_code=da.trust_remote_code
             )
 
         tokens = []
@@ -38,3 +42,68 @@ class TokenEstimator2(TokenEstimator):
 
     def get_num_samples(self):
         return len(self.tokens)
+
+def ExtractTokenEstimator2Contract(da: DataArguments):
+    if da.dataset is None:
+        raise RuntimeError("Dataset argument has to be filled in for TE2!")
+
+    if da.dataset.endswith(".json") or da.dataset.endswith(".jsonl"):
+        logger.info("Parsing dataset as local json file")
+        dataset = load_dataset("json", data_files={"train": da.dataset})["train"]
+    else:
+        dataset = load_dataset(
+            da.dataset, name=da.dataset_config_name, split=da.dataset_split
+        )
+
+    tokens = []
+    print("Loading data in dataset...")
+    for item in tqdm(dataset):
+        tokens.append(int(len(item[da.dataset_text_field]) / 3.6))
+
+    tokens = np.sort(tokens)[::-1]
+
+    contract = {}
+    contract["len"] = len(tokens)
+    contract["total"] = int(np.sum(tokens))
+    contract["min"] = int(np.min(tokens))
+    contract["max"] = int(np.max(tokens))
+    contract["mean"] = round(np.mean(tokens), 2)
+    contract["std"] = round(np.std(tokens), 2)
+
+    contract["bs1"] = contract["mean"]
+
+    # for bs = 2, 4, 6, 8, 16
+    batch_sizes = [2**i for i in range(1, 5) if 2**i <= contract["len"]]
+    for bs in batch_sizes:
+        contract[f"bs{bs}"] = np.mean(tokens[:int(len(tokens)/bs)])
+
+    return contract
+
+class TokenEstimator2FromContract:
+    def __init__(self, contract: dict):
+        self.contract = contract
+
+        m = {}
+        m[1] = contract["bs1"]
+    
+        batch_sizes = [2**i for i in range(1, 5) if 2**i <= contract["len"]]
+        for bs in batch_sizes:
+            m[bs] = contract[f"bs{bs}"]
+
+        X = np.array([[i] for i in m.keys()])
+        y = np.array(list(m.values()))
+
+        self.m = m
+        self.reg = LinearRegression().fit(X, y)
+
+    def get_total_tokens(self):
+        return self.contract["total"]
+
+    def get_estimated_batch_width(self, bs):
+        if bs in self.m.keys():
+            return self.m[bs]
+
+        return self.reg.predict([[bs]])[0]
+
+    def get_num_samples(self):
+        return self.contract["len"]
