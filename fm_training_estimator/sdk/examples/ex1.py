f00b410ba317e3a6747d7a902e32eccccf4343e2
Angel Luu <angel.luu@us.ibm.com>
2024-10-22 16:49:04 -0600

    refactor: Use the new dataclass for estimate input to sdk
    Signed-off-by: Angel Luu <angel.luu@us.ibm.com>

--- fm_training_estimator/sdk/examples/ex1.py
+++ fm_training_estimator/sdk/examples/ex1.py
@@ -3,6 +3,7 @@ import os
 
 # First Party
 from fm_training_estimator.config.arguments import (
+<<<<<<< HEAD
     DataArguments,
     EstimateInput,
     EstimatorMetadata,
@@ -17,10 +18,19 @@ from fm_training_estimator.sdk import (
     estimate_time,
     estimate_tokens,
 )
+=======
+    EstimateInput,
+    FMArguments,
+    HFTrainingArguments,
+    JobConfig,
+)
+from fm_training_estimator.sdk import estimate_memory
+>>>>>>> cc90e49 (refactor: Use the new dataclass for estimate input to sdk)
 
 workdir_path = os.path.join(os.path.abspath(os.curdir), "workdir")
 
 model_path = os.path.join(workdir_path, "model.json")
+<<<<<<< HEAD
 lookup_data_path = os.path.join(workdir_path, "data.csv")
 
 estimator_metadata = EstimatorMetadata(base_data_path=lookup_data_path)
@@ -57,3 +67,27 @@ print("With only theory: ", estimate_time(est_input))
 print("Estimating Tokens:....")
 print("With only theory: ", estimate_tokens(est_input))
 # print("With reg model: ", estimate_tokens(est_input, model_path))
+=======
+fm = FMArguments(
+    base_model_path="ibm-granite/granite-7b-base",
+    torch_dtype="float16",
+    block_size=512,
+)
+hf_training = HFTrainingArguments(
+    per_device_train_batch_size=4,
+)
+job_conf = JobConfig(
+    hf_training,
+    fm,
+)
+est_input = EstimateInput(job_configs=[job_conf])
+
+print("With only theory: ", estimate_memory(est_input))
+print("With reg model: ", estimate_memory(est_input, model_path))
+
+hf_training.fsdp = "full_shard"
+
+print("Using fsdp full shard")
+print("With only theory: ", estimate_memory(est_input))
+# print("With reg model: ", estimate_memory(est_input, model_path))
+>>>>>>> cc90e49 (refactor: Use the new dataclass for estimate input to sdk)
