cc90e49d9096f95853ea79a4fed65aca256e83c9
Angel Luu <angel.luu@us.ibm.com>
2024-10-22 16:49:04 -0600

    refactor: Use the new dataclass for estimate input to sdk
    Signed-off-by: Angel Luu <angel.luu@us.ibm.com>

--- fm_training_estimator/sdk/examples/ex1.py
+++ fm_training_estimator/sdk/examples/ex1.py
@@ -1,19 +1,37 @@
+# Standard
+import os
+
 # First Party
+from fm_training_estimator.config.arguments import (
+    EstimateInput,
+    FMArguments,
+    HFTrainingArguments,
+    JobConfig,
+)
 from fm_training_estimator.sdk import estimate_memory
 
-model_path = "../../../workdir/model.json"
+workdir_path = os.path.join(os.path.abspath(os.curdir), "workdir")
 
-config = {
-    "base_model_path": "ibm-granite/granite-7b-base",
-    "torch_dtype": "float16",
-    "per_device_train_batch_size": 4,
-    "block_size": 512,
-}
+model_path = os.path.join(workdir_path, "model.json")
+fm = FMArguments(
+    base_model_path="ibm-granite/granite-7b-base",
+    torch_dtype="float16",
+    block_size=512,
+)
+hf_training = HFTrainingArguments(
+    per_device_train_batch_size=4,
+)
+job_conf = JobConfig(
+    hf_training,
+    fm,
+)
+est_input = EstimateInput(job_configs=[job_conf])
 
-print("With only theory: ", estimate_memory(config))
-print("With reg model: ", estimate_memory(config, model_path))
+print("With only theory: ", estimate_memory(est_input))
+print("With reg model: ", estimate_memory(est_input, model_path))
 
-config["fsdp"] = "full_shard"
+hf_training.fsdp = "full_shard"
 
-print("With only theory: ", estimate_memory(config))
-print("With reg model: ", estimate_memory(config, model_path))
+print("Using fsdp full shard")
+print("With only theory: ", estimate_memory(est_input))
+# print("With reg model: ", estimate_memory(est_input, model_path))
--- fm_training_estimator/sdk/sdk.py
+++ fm_training_estimator/sdk/sdk.py
@@ -1,37 +1,60 @@
+# First Party
+from fm_training_estimator.config.arguments import EstimateInput, MemoryEstimate
+from fm_training_estimator.memory.hybrid.hybrid import HybridEstimator
+from fm_training_estimator.memory.lora.hybrid import HybridLoraEstimator
+
 # Local
 from ..config import is_fsdp, parse
-from ..memory import HybridEstimator
 from ..utils import fmt_size
 
 
-def estimate_memory(config, reg_model=None):
+def estimate_memory(
+    estimate_input: EstimateInput, model_path: str = None
+) -> MemoryEstimate:
     """
-    If no reg model is passed in, falls back to pure theory approach.
+    Use hybdrid model by default.
+    If no reg model is passed in (check in estimate_input.estimator_metadata), falls back to theory approach.
     """
-    res = {}
+    if estimate_input.job_configs is None or len(estimate_input.job_configs) == 0:
+        raise ValueError("Did not receive a training job config")
+
+    # Only going to process first job_config for now
+    job_config = estimate_input.job_configs[0]
 
     # disable any token estimation
-    config["te_approach"] = -1
-    # set num gpus if needed
-    if "numGpusPerPod" not in config:
-        config["numGpusPerPod"] = 0
+    job_config.data.te_approach = -1
 
-    fm, ta, ia, _ = parse(config)
-    est = HybridEstimator(fm, ta, ia, None, reg_model)
+    if job_config.fm.technique == "lora":
+        est = HybridLoraEstimator(
+            job_config.fm,
+            job_config.hf_training,
+            job_config.infra,
+            job_config.peft_lora,
+            None,
+            model_path,
+        )
+    else:
+        est = HybridEstimator(
+            job_config.fm, job_config.hf_training, job_config.infra, None, model_path
+        )
 
-    res["total_mem_estimate_og"] = est.get_total_mem_estimate()
-    res["activation_memory_og"] = est.calculate_activation_memory()
-    res["gradient_memory_og"] = est.calculate_gradient_memory()
-    res["model_memory_og"] = est.calculate_model_memory()
-    res["optimizer_memory_og"] = est.calculate_optimizer_memory()
+    total_mem_estimate = fmt_size(est.get_total_mem_estimate())
+    activation_memory = fmt_size(est.calculate_activation_memory())
+    gradient_memory = fmt_size(est.calculate_gradient_memory())
+    model_memory = fmt_size(est.calculate_model_memory())
+    optimizer_memory = fmt_size(est.calculate_optimizer_memory())
 
-    res["total_mem_estimate"] = fmt_size(res["total_mem_estimate_og"])
-    res["activation_memory"] = fmt_size(res["activation_memory_og"])
-    res["gradient_memory"] = fmt_size(res["gradient_memory_og"])
-    res["model_memory"] = fmt_size(res["model_memory_og"])
-    res["optimizer_memory"] = fmt_size(res["optimizer_memory_og"])
+    if is_fsdp(job_config.hf_training):
+        num_gpus = est.fsdp_est.get_number_of_gpus()
+    else:
+        num_gpus = 0
+        # logger.info("Not enough info to estimate num_gpus, setting to 0.")
 
-    if is_fsdp(ta):
-        res["num_gpus"] = est.fsdp_est.get_number_of_gpus()
-
-    return res
+    return MemoryEstimate(
+        total_mem_estimate,
+        activation_memory,
+        gradient_memory,
+        model_memory,
+        optimizer_memory,
+        num_gpus,
+    )
