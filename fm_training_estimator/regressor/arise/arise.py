4f734303576de59a6f4258f1eb1f22f98395e9a3
ChanderG <mail@chandergovind.org>
2025-02-08 15:45:35 +0530

    feat(arise): arise model training with new integration mech

--- fm_training_estimator/regressor/arise/arise.py
+++ fm_training_estimator/regressor/arise/arise.py
@@ -4,12 +4,53 @@ import shutil
 import pandas
 import yaml
 
-from arise_predictions.sdk import execute_auto_build_models, get_base_args, execute_predict
+# from arise_predictions.sdk import execute_auto_build_models, get_base_args, execute_predict
+
+from arise_predictions.preprocessing import job_parser
+from arise_predictions.utils import constants
+from arise_predictions.auto_model.build_models import auto_build_models, get_estimators_config
+
 from ...data import get_format_by_version
 
 class AriseRegressor:
     def __init__(self, model_path=None):
         self.model_path = model_path
+
+    def preprocess(self, workdir, job_spec):
+        inputs = sorted(list(job_spec[0]))
+        outputs = sorted(list(job_spec[1]))
+        start_time_field_name = job_spec[2]
+        end_time_field_name = job_spec[3]
+        job_parser_class_name = job_spec[4]
+        job_entry_filter = job_spec[5]
+        feature_engineering = job_spec[6] if len(job_spec) > 6 else None
+        metadata_parser_class_name = job_spec[7] if len(job_spec) > 7 else None
+        history_file = os.path.join(workdir, constants.JOB_HISTORY_FILE_NAME + ".csv")
+
+        history_data, history_file = job_parser.collect_jobs_history(
+                os.path.join(workdir, constants.JOB_DATA_DIR), workdir, inputs, outputs,
+                start_time_field_name, end_time_field_name, None, job_parser_class_name,
+                job_entry_filter, feature_engineering, metadata_parser_class_name,
+                workdir)
+        return history_data, history_file
+
+    def execute_build(self, workdir, js, config_path):
+
+        history_data, history_file = self.preprocess(workdir, js)
+        outputs = sorted(list(js[1]))
+        output_path = os.path.join(workdir, constants.AM_OUTPUT_PATH_SUFFIX)
+
+        auto_build_models(raw_data=history_data,
+                          config=get_estimators_config(config_path, num_jobs=-1),
+                          target_variables=outputs,
+                          output_path=output_path,
+                          leave_one_out_cv=None,
+                          feature_col=None,
+                          low_threshold=None,
+                          high_threshold=None,
+                          single_output_file=True,
+                          randomized_hpo=False,
+                          n_random_iter=False)
 
     def train(self, data_path: str, model_path: str, config_path: str, y_headers: list[str]):
         with tempfile.TemporaryDirectory() as workdir:
@@ -27,17 +68,10 @@ class AriseRegressor:
             job_spec = {"job-metadata-inputs": {}, "job-metadata-outputs": y_headers}
             for h in x_headers:
                 job_spec["job-metadata-inputs"][h] = 0
-            job_file = os.path.join(workdir, "job_spec.yaml")
-            with open(job_file, "w") as jobfile:
-                yaml.dump(job_spec, jobfile)
 
-            args = get_base_args("auto-build-models")
-            args.input_path = workdir
-            args.reread_history = True
-            args.single_output_file = True
-            args.config_file = config_path
+            js = job_parser.parse_job_spec(job_spec)
 
-            execute_auto_build_models(args)
+            self.execute_build(workdir, js, config_path)
 
             # copy the model to required destination
             shutil.copy2(os.path.join(workdir, "ARISE-auto-models.zip"), model_path)
